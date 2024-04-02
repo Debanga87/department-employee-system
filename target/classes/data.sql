9eb53454a19b1327867f7209e20a0b7df68de09c
Debanga Bhuyan <debangabhuyan007@gmail.com>
2024-04-02 13:29:18 +0530

    Update data.sql files

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO department (name) VALUES ('Department A');
-INSERT INTO department (name) VALUES ('Department B');
-INSERT INTO department (name) VALUES ('Department C');
+INSERT INTO departments (name) VALUES ('Department A');
+INSERT INTO departments (name) VALUES ('Department B');
+INSERT INTO departments (name) VALUES ('Department C');
 
-INSERT INTO employee (name) VALUES ('John Doe');
-INSERT INTO employee (name) VALUES ('Jane Smith');
+INSERT INTO employees (name) VALUES ('John Doe');
+INSERT INTO employees (name) VALUES ('Jane Smith');
